7c6d8403ab49fc96e6a223ce48108369fe5683e7
Cezary Salbut <csalbut@gmail.com>
2016-05-14 08:58:47 +0200

    remove redundant parens

--- logging.h
+++ logging.h
@@ -1,12 +1,12 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#define LOG_FATAL    (1)
-#define LOG_ERR      (2)
-#define LOG_WARN     (3)
-#define LOG_INFO     (4)
-#define LOG_DBG      (5)
-
+#define LOG_NONE     0
+#define LOG_FATAL    1
+#define LOG_ERR      2
+#define LOG_WARN     3
+#define LOG_INFO     4
+#define LOG_DBG      5
 
 #define LOG(level, str, args...) \
     do {  \
