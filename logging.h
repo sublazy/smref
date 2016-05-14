c941bb54be42003e5e191c48c503ba1e027c47b0
Cezary Salbut <csalbut@gmail.com>
2016-05-14 08:21:36 +0200

    add multi-level debugging log macros

---
+++ logging.h
@@ -0,0 +1,19 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#define LOG_FATAL    (1)
+#define LOG_ERR      (2)
+#define LOG_WARN     (3)
+#define LOG_INFO     (4)
+#define LOG_DBG      (5)
+
+extern int debug_level;
+
+#define LOG(level, str, args...) \
+    do {  \
+        if (level <= debug_level) { \
+            printf("LOG: "str"\r\n", ##args); \
+        } \
+    } while (0)
+
+#endif // LOGGING_H
--- main.c
+++ main.c
@@ -3,22 +3,26 @@
 #include "sm.h"
 #include "sm_tlc.h"
 #include "sm_tlc2.h"
+#include "logging.h"
+
+int debug_level = LOG_INFO;
 
 int main(void)
 {
-    printf("state machine test\n");
-
+    LOG(LOG_INFO, "Entering main");
     sm_t *sm_tlc = sm_tlc_get_obj();
     sm_t *sm_tlc2 = sm_tlc2_get_obj();
     sm_print_tx_table(sm_tlc);
+    int cnt = 0;
 
     while(1) {
-        puts(".");
+        LOG(LOG_DBG, "--- Loop cycle %d", cnt);
         sm_run(sm_tlc);
         sm_run(sm_tlc2);
         sleep(1);
         sm_send_event(sm_tlc, TLC_EVENT_NEXT);
         sm_send_event(sm_tlc2, TLC_EVENT_NEXT);
+        cnt++;
     }
 
     return 0;
