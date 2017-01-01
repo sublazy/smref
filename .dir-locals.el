71362b55b4f59bc6c785c2540d952adfe8472f19
Cezary Sałbut <csalbut@gmail.com>
2017-01-02 00:58:36 +0100

    add code formatting settings for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil .
+      ((evil-shift-width . 4)
+       (tab-width . 4)
+       (indent-tabs-mode . nil)
+       (evil-indent-convert-tabs . t)))
+ (c-mode .
+         ((c-file-style . "user"))))
