56e92f5b719179ec42a4a89eae1fe64113bb82e7
Cezary Sałbut <csalbut@gmail.com>
2017-01-04 18:17:42 +0100

    disable all but one state machine

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-sources = main.c sm.c sm_tlc.c sm_tlc_actions.c sm_car.c sm_tlc2.c
+sources = main.c sm.c sm_tlc.c sm_tlc_actions.c
 
 all: $(sources) Makefile
 	gcc --std=c99 -Wall $(sources) -o main
--- main.c
+++ main.c
@@ -10,13 +10,13 @@ static int debug_level = LOG_NONE;
 int main(void)
 {
     sm_t *sm_tlc = sm_tlc_get_obj();
-    sm_t *sm_car = sm_car_get_obj();
+    //sm_t *sm_car = sm_car_get_obj();
 
     int cnt = 0;
     while(1) {
         LOG(LOG_DBG, "--- Loop cycle %d", cnt);
         sm_run(sm_tlc);
-        sm_run(sm_car);
+        //sm_run(sm_car);
         sleep(1);
         sm_send_event(sm_tlc, TLC_EVENT_NEXT);
         cnt++;
